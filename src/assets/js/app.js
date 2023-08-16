685f67c778ccdef8cdc2910e6dff2aae82f5dc97
lucas-espi <lucass.espi@gmail.com>
2023-08-16 20:56:08 -0300

    corrigiendo json

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,61 +1,41 @@
-// CONECCIÓN CON EL DOM
+// CONEXIÓN CON EL DOM
 
 const boxPorfolio = document.getElementById("box-porfolio");
 
+const proyectos = [];
+const URL = "/src/assets/js/bbdd/bbdd.json";
 
-const proyectos = [
-    {
-        titulo: "Eureka Artesanal",
-        imagen:"https://imgur.com/fglU0ex",
-        descripcion:'Descripción breve del proyecto.',
-        urlProyecto: 'https://lucas-espi.github.io/Proyecto_final-Eureka/',
-        urlGithub: 'https://github.com/lucas-espi/Proyecto_final-Eureka'
-    },
-    {
-        titulo: "Camino Aventura",
-        imagen:"https://imgur.com/rAvZOUp",
-        descripcion:'Descripción breve del proyecto.',
-        urlProyecto: 'https://lucas-espi.github.io/ProyectoFinal-JS/',
-        urlGithub: 'https://github.com/lucas-espi/ProyectoFinal-JS'
-    },
-    {
-        titulo: "Camino Aventura II",
-        imagen:"https://imgur.com/cM4P5Qk",
-        descripcion:'Descripción breve del proyecto.',
-        urlProyecto: 'https://camino-aventura.netlify.app',
-        urlGithub: 'https://github.com/lucas-espi/Camino-Aventura-React'
-    },
-    {
-        titulo: "Camino Aventura II",
-        imagen:"https://imgur.com/cM4P5Qk",
-        descripcion:'Descripción breve del proyecto.',
-        urlProyecto: 'https://camino-aventura.netlify.app',
-        urlGithub: 'https://github.com/lucas-espi/Camino-Aventura-React'
+// Conexión fetch
+const cargarContenido = async () => {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        proyectos.push(...data);
+        createCard(proyectos); // Pasar 'proyectos' en lugar de '...data'
+    } catch (error) {
+        console.log('Error:', error);
     }
-]
+};
+cargarContenido();
 
-// Funcion para itertar array/json (proyectos) y crear elementos HTML dinámicamente con JS
-
+// Función para iterar array/json (proyectos) y crear elementos HTML dinámicamente con JS
 const createCard = (array) => {
     boxPorfolio.innerHTML = '';
 
-    array.forEach( (pro) => {
+    array.forEach((pro) => {
         let div = document.createElement('div');
-        div.setAttribute ("class", "portfolio-card-box");
-            div.innerHTML +=
+        div.setAttribute("class", "portfolio-card-box");
+        div.innerHTML +=
             `
             <img src="${pro.imagen}" class="img-card" alt="Previsualización del Proyecto">
             <h2 class="h2-card">${pro.titulo}</h2>
             <p class="p-card">${pro.descripcion}</p>
             <div class="links">
-                <a href="${pro.urlProyecto}" class="btn-url" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square"></i> Visitar</a> 
+                <a href="${pro.urlProyecto}" class="btn-url" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square"></i> Visitar</a>
                 <a href="${pro.urlGithub}" class="btn-github" target="_blank"> <i class="fa-brands fa-github"></i>GitHub</a>
             </div>
-            ` 
-            boxPorfolio.appendChild(div)
+            `;
+        boxPorfolio.appendChild(div);
     });
-}
+};
 
-createCard(proyectos);
-
-// console.log('probando js')
