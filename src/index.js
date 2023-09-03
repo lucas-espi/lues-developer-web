782066db020c0300ce7c54e928bbad67978bd198
lucas-espi <lucass.espi@gmail.com>
2023-09-03 20:32:48 -0300

    modificando ruta de inde.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ const path = require('path')
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(require('./routes/index'))
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.get('/', (req, res) => {
+    // Envia el archivo index.html ubicado en la raíz del proyecto
+    res.sendFile(path.join(__dirname, '../index.html'));
+  });
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000");
