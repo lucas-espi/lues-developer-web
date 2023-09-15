386f82b5166f4d018f1359cbf11a873dd6535f5f
lucas-espi <lucass.espi@gmail.com>
2023-09-15 18:10:20 -0300

    primer intento configurar node fallado

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ const path = require('path')
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(require('./routes/index'))
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    // Envia el archivo index.html ubicado en la raíz del proyecto
-    res.sendFile(path.join(__dirname, '../index.html'));
-  });
+  // Envia el archivo index.html ubicado en la raíz del proyecto
+  res.sendFile(path.join(__dirname, '../index.html'));
+});;
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
